0543986bb30f2bbde411477a33a8542914fd2b02
Leah86 <Niava@users.noreply.github.com>
2017-10-17 14:15:06 +1100

    complete hold function and use dry coding to minimise lines of code

--- app.js
+++ app.js
@@ -43,29 +43,56 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
 
 	}else{
 		//next player
-		//if activeplayer = 0 then player =1, else player=0;
-		activePlayer  === 0 ? activePlayer = 1 : activePlayer = 0;
-		roundScore = 0;
-
-		document.getElementById('current-0').textContent = "0";
-		document.getElementById('current-1').textContent = "0";
-
-		//document.querySelector('.player-0-panel').classList.remove('active');
-		//document.querySelector('.player-1-panel').classList.add('active');
-
-		document.querySelector('.player-0-panel').classList.toggle('active');
-		document.querySelector('.player-1-panel').classList.toggle('active');
-
-		//change dice number to none when switch player
-		document.querySelector('.dice').style.display='none';
+		nextPlayer();
 
 	}
 
 });
 
+document.querySelector('.btn-hold').addEventListener('click',function(){
+	//add current score to global score
+	scores[activePlayer] += roundScore;
+
+
+	//update the ui
+	document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
+
+	//check if player won the game
+	if(scores[activePlayer] >= 20){
+		document.querySelector('#name-'+activePlayer).textContent = "Winner!";
+		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+		document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+	} else {
+		//next player
+		nextPlayer();
+	}
+
+	
+
+});
 
 
 
+function nextPlayer(){
+	//next player
+	//if activeplayer = 0 then player =1, else player=0;
+	activePlayer  === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
+
+	document.getElementById('current-0').textContent = "0";
+	document.getElementById('current-1').textContent = "0";
+
+	//document.querySelector('.player-0-panel').classList.remove('active');
+	//document.querySelector('.player-1-panel').classList.add('active');
+
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+
+	//change dice number to none when switch player
+	document.querySelector('.dice').style.display='none';
+
+}
 
 
 
