24edb092b0b5f895d5c9ac43f8fb4fe736d3ee63
Leah86 <Niava@users.noreply.github.com>
2017-10-17 12:04:59 +1100

    update scores and change active player using toggle

--- app.js
+++ app.js
@@ -36,7 +36,30 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
 	diceDOM.src='dice-'+dice+'.png';
 
 	//3. update the round score if the roll number was not a 1
+	if(dice !== 1){
+		//add score
+		roundScore += dice;
+		document.querySelector('#current-'+activePlayer).textContent = roundScore;
 
+	}else{
+		//next player
+		//if activeplayer = 0 then player =1, else player=0;
+		activePlayer  === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+
+		document.getElementById('current-0').textContent = "0";
+		document.getElementById('current-1').textContent = "0";
+
+		//document.querySelector('.player-0-panel').classList.remove('active');
+		//document.querySelector('.player-1-panel').classList.add('active');
+
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+
+		//change dice number to none when switch player
+		document.querySelector('.dice').style.display='none';
+
+	}
 
 });
 
